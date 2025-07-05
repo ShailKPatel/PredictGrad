274d837f6abd126256980ea68f12b9db48778d58
Shail K Patel <shailpatel.connect@gmail.com>
2025-07-06 00:17:10 +0530

    added demo [age

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # Define pages 
 home = st.Page("pages/Welcome.py", icon='🏠')
 predict_student_risk = st.Page("pages/Predict_Student_Risk.py", icon='🔮') # for prediction
+demo = st.Page("pages/Demo_Predict_Student_Risk.py", icon='🚄') # for prediction
 feature_impact = st.Page("pages/Feature_Impact.py", icon='🎯') # For SHAP
 data_insights = st.Page("pages/Data_Insights.py", icon='📈') # For EDA
 feedback = st.Page("pages/Feedback.py", icon='💬') # for feedback
@@ -11,7 +12,7 @@ credits = st.Page("pages/Credits.py", icon='📜') # for Credits
 # Group pages
 pg = st.navigation({
     "Home": [home],
-    "Analysis & Insights": [predict_student_risk, feature_impact, data_insights], # Grouped prediction, SHAP, and EDA
+    "Analysis & Insights": [predict_student_risk, demo, feature_impact, data_insights], # Grouped prediction, SHAP, and EDA
     "About": [feedback, credits] # Grouped feedback and credits
 })
 
