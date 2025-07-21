442c86d36f5f59a941185b8b416c94eedf456b41
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2025-07-21 22:49:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 home = st.Page("pages/Welcome.py", icon='🏠')
 predict_student_risk = st.Page("pages/Predict_Student_Risk.py", icon='🔮') # for prediction
 demo = st.Page("pages/Demo_Predict_Student_Risk.py", icon='🚄') # for prediction
-feature_impact = st.Page("pages/Feature_Impact.py", icon='🎯') # For SHAP
+feature_impact = st.Page("pages/Model_Analysis.py", icon='🎯') # For SHAP
 data_insights = st.Page("pages/Data_Insights.py", icon='📈') # For EDA
 feedback = st.Page("pages/Feedback.py", icon='💬') # for feedback
 credits = st.Page("pages/Credits.py", icon='📜') # for Credits
