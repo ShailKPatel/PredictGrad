b76ed916819f98aac6683647141361981d59a0cc
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2025-07-24 17:08:53 +0530

    Updated app.py to add new page

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ import streamlit as st
 
 # Define pages 
 home = st.Page("pages/Welcome.py", icon='🏠')
+
 predict_student_risk = st.Page("pages/Predict_Student_Risk.py", icon='🔮') # for prediction
-demo = st.Page("pages/Demo_Predict_Student_Risk.py", icon='🚄') # for prediction
-feature_impact = st.Page("pages/Model_Analysis.py", icon='🎯') # For SHAP
+demo = st.Page("pages/Demo_Predict_Student_Risk.py", icon='🎓') # Demo prediction
+
 data_insights = st.Page("pages/Data_Insights.py", icon='📈') # For EDA
+model_selection = st.Page("pages/Model_Selection.py", icon='⚙️') # For model selection
+model_analysis = st.Page("pages/Model_Analysis.py", icon='🎯') # For SHAP
+
 feedback = st.Page("pages/Feedback.py", icon='💬') # for feedback
 credits = st.Page("pages/Credits.py", icon='📜') # for Credits
 
 # Group pages
 pg = st.navigation({
     "Home": [home],
-    "Analysis & Insights": [predict_student_risk, demo, feature_impact, data_insights], # Grouped prediction, SHAP, and EDA
+    "Predict": [predict_student_risk, demo], # Grouped prediction
+    "Insights": [data_insights, model_selection, model_analysis], # Model selection
     "About": [feedback, credits] # Grouped feedback and credits
 })
 
